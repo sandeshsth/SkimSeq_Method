c2682a336a3ab61b3102a5fdd24e0141855d7a18
Laxman Adhikari <49244360+laxmangene7@users.noreply.github.com>
2021-01-22 09:35:16 -0600

    Update Data_Visualization_Read_Count_Graph.R

--- Data_Visualization_Read_Count_Graph.R
+++ Data_Visualization_Read_Count_Graph.R
@@ -1,12 +1,18 @@
 ######### R script to generate read count vs. position graph for skim-seq population ######
 
-f4 <- fread("DNA191014P04_B11.txt", header = T, check.names = T, data.table = F)
-colnames(df4)[1:5] = c('rawread', 'chr', 'pos', 'nread', 'sample' ) ## rawread = read count; chr = chromosome; pos = position; nread = normalized read
-head(df4)
-p <- ggplot(data = subset(df4, df4$nread<230), aes(pos,nread)) + geom_point(size = 0.4, colour="blue") + facet_wrap( ~ chr, ncol=1 ) +   ## ,scales = "free_x"
+df4 <- fread("DNA191014P04_B11.TA5798.txt", header = T, check.names = T, data.table = F)
+colnames(df4)[1:5] = c('rawread', 'chr', 'pos', 'nread', 'sample' )
+#head(df4)
+p <- ggplot(data = subset(df4, df4$nread<200), aes(pos,nread)) + geom_point(size = 0.8, colour="blue") + facet_wrap( ~ chr, ncol=1, strip.position = "right" ) +   ## ,scales = "free_x"
   scale_x_continuous(labels=function(x)x/1000000, breaks = seq(0,900000000, by = 100000000),expand = c(0.01, 0)) + xlab("Genomic Position (Mb)") + ylab("Normalized Read Count")+
-  theme(panel.background = element_blank(),
-        panel.grid.major = element_line(size = 0.001, linetype = 'solid',colour = "gray88"),   ##
- axis.line = element_line(colour = "black")
+  theme(panel.background = element_rect(fill="white"),
+        panel.spacing = unit(1.5, "lines"),
+        strip.text.y.right = element_text(angle = 0, face="bold", size="14"),
+        panel.grid.major = element_line(size = 0.0001, linetype = 'solid',colour = "gray95"), 
+    panel.border=element_rect(colour="black",size=0.1, fill=NA), 
+ axis.text = element_text( size = 10, face="bold" ),
+ axis.title = element_text( size = 14, face = "bold" ),
+ strip.text = element_text(size = 20)
+
   )
-ggsave("DNA191014P04_B11.col.new.github2.pdf", p, width = 6, height = 5, units = "in", limitsize = F) 
+ggsave("DNA191014P04_B11.TA5798.pdf", p, width = 8, height = 5.5, units = "in", limitsize = F) 
